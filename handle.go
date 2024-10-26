0c0f1ef5cdd58ccff7e513534f438ff9f3134ca0
Mallknow <jamlinkedlane@163.com>
2024-10-26 23:46:36 +0800

    fix api response errorcode bug

--- handle.go
+++ handle.go
@@ -1,20 +1,22 @@
 package cmcapi
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type CmcStatusRes struct {
 	Status struct {
-		Timestamp    string `json:"timestamp"`
-		ErrorCode    int    `json:"error_code"`
-		ErrorMessage string `json:"error_message"`
-		Elapsed      int    `json:"elapsed"`
-		CreditCount  int    `json:"credit_count"`
+		Timestamp    string      `json:"timestamp"`
+		ErrorCode    interface{} `json:"error_code"`
+		ErrorMessage string      `json:"error_message"`
+		Elapsed      int         `json:"elapsed"`
+		CreditCount  int         `json:"credit_count"`
 	} `json:"status"`
 }
 
 type CmcRestRes[T any] struct {
-	Data         T `json:"data"` // 请求结果
 	CmcStatusRes   // 状态信息
+	Data         T `json:"data"` // 请求结果
 }
 
 func handlerCommonRest[T any](data []byte) (*CmcRestRes[T], error) {
@@ -22,13 +24,15 @@ func handlerCommonRest[T any](data []byte) (*CmcRestRes[T], error) {
 	log.Warn(string(data))
 	err := json.Unmarshal(data, &res)
 	if err != nil {
-		log.Error("rest返回值获取失败", err)
+		log.Error("rest返回值获取失败: ", err)
 	}
 	return res, err
 }
-func (status *CmcStatusRes) handlerError() error {
-	if status.Status.ErrorCode != 0 {
-		return fmt.Errorf("request error:[code:%v] message: %v", status.Status.ErrorCode, status.Status.ErrorMessage)
+func (s *CmcStatusRes) handlerError() error {
+	// API Response Bug: Status ErrorCode could be int or string
+	errorCode := fmt.Sprintf("%v", s.Status.ErrorCode)
+	if errorCode != "0" {
+		return fmt.Errorf("request error:[code:%v] message: %v", s.Status.ErrorCode, s.Status.ErrorMessage)
 	}
 	return nil
 }
--- rest_res_fearandgread.go
+++ rest_res_fearandgread.go
@@ -1,26 +1,5 @@
 package cmcapi
 
-// value
-// integer [ 0 .. 100 ]
-// The value of CMC Fear and Greed.
-//
-// When the value is closer to 0, the market is in Extreme Fear, and investors have over-sold irrationally.
-//
-// When the value is closer to 100, the market is in Extreme Greed, indicating a likely market correction.
-//
-// value_classification
-// string
-// The value classication of CMC Fear and Greed.
-//
-// 1 ≤ x < 20: Extreme Fear
-// 20 ≤ x < 40: Fear
-// 40 ≤ x < 60: Neutral
-// 60 ≤ x < 80: Greed
-// 80 ≤ x ≤ 100: Extreme Greed
-//
-// update_time
-// string <date>
-// Timestamp (ISO 8601) of the last time this record was updated.
 type CmcRestApiFearAndGreedLatestRes struct {
 	Value               int    `json:"value"`                //integer [ 0 .. 100 ] The value of CMC Fear and Greed.
 	UpdateTime          string `json:"update_time"`          //string <date> Timestamp (ISO 8601) of the last time this record was updated.
